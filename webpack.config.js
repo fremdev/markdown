5e8b85805f96f9aacd0de7eecd4890f487a8fd52
Alex Efremov <alex.efremov.dev@gmail.com>
2016-07-18 23:46:09 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './app',
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: [ 'style', 'css', 'sass' ]
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
